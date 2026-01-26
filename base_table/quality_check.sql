62d87c0b960fd80481fcaa75f46650eeb2666125
cleopatra563 <cleopatrazhou563@gmail.com>
2026-01-26 19:46:37 +0800

    1

--- base_table/quality_check.sql
+++ base_table/quality_check.sql
@@ -1,4 +1,3 @@
-
 -- 数据质量检查
     -- 1,重复行（数据膨胀）、缺失行（数据缺失）
 
@@ -7,7 +6,6 @@ select role_id,log_date,count(*)
 from active_ad
 group by 1,2
 having count(*)>1
-
 
 -- 左表原本行
 select count(*)
@@ -28,8 +26,4 @@ from active t1
 left join ad t2
     on t1.role_id=t2.role_id
 group by 1,2
-having count(*) >1 
-
-
-t1.role_id
-,t1.log_date 
+having count(*) >1
